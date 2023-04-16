f6467fa3862fee4db8492e9ae5e405f2e441981c
NdekoCode <arickbulakali@gmail.com>
2023-04-17 01:19:51 +0200

    refactor: add utilities functions file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,216 +1,43 @@
-// File Upload
-function ekUpload() {
-  function Init() {
-    console.log("Upload Initialised");
-
-    const fileSelect = document.getElementById("file-upload"),
-      fileDrag = document.getElementById("file-drag"),
-      submitButton = document.getElementById("submit-button");
-
-    fileSelect.addEventListener("change", fileSelectHandler, false);
-
-    // Is XHR2 available?
-    const xhr = new XMLHttpRequest();
-    if (xhr.upload) {
-      // File Drop
-      fileDrag.addEventListener("dragover", fileDragHover, false);
-      fileDrag.addEventListener("dragleave", fileDragHover, false);
-      fileDrag.addEventListener("drop", fileSelectHandler, false);
-    }
-  }
-
-  function fileDragHover(e) {
-    const fileDrag = document.getElementById("file-drag");
-
-    e.stopPropagation();
-    e.preventDefault();
-
-    fileDrag.className =
-      e.type === "dragover" ? "hover" : "modal-body file-upload";
-  }
-
-  function fileSelectHandler(e) {
-    // Fetch FileList object
-    const files = e.target.files || e.dataTransfer.files;
-
-    // Cancel event and hover styling
-    fileDragHover(e);
-
-    // Process all File objects
-    for (let i = 0, f; (f = files[i]); i++) {
-      parseFile(f);
-      uploadFile(f);
-    }
-  }
-
-  // Output
-  function output(msg) {
-    // Response
-    const m = document.getElementById("messages");
-    m.innerHTML = msg;
-  }
-
-  function parseFile(file) {
-    console.log(file.name);
-    output("<strong>" + encodeURI(file.name) + "</strong>");
-
-    // const fileType = file.type;
-    // console.log(fileType);
-    const imageName = file.name;
-
-    const isGood = /\.(?=gif|jpg|png|jpeg)/gi.test(imageName);
-    if (isGood) {
-      document.getElementById("start").classList.add("hidden");
-      document.getElementById("response").classList.remove("hidden");
-      document.getElementById("notimage").classList.add("hidden");
-      // Thumbnail Preview
-      document.getElementById("file-image").classList.remove("hidden");
-      document.getElementById("file-image").src = URL.createObjectURL(file);
-    } else {
-      document.getElementById("file-image").classList.add("hidden");
-      document.getElementById("notimage").classList.remove("hidden");
-      document.getElementById("start").classList.remove("hidden");
-      document.getElementById("response").classList.add("hidden");
-      document.getElementById("file-upload-form").reset();
-    }
-  }
-
-  function setProgressMaxValue(e) {
-    const pBar = document.getElementById("file-progress");
-
-    if (e.lengthComputable) {
-      pBar.max = e.total;
-    }
-  }
-
-  function updateFileProgress(e) {
-    const pBar = document.getElementById("file-progress");
-
-    if (e.lengthComputable) {
-      pBar.value = e.loaded;
-    }
-  }
-
-  function uploadFile(file) {
-    const xhr = new XMLHttpRequest(),
-      fileInput = document.getElementById("class-roster-file"),
-      pBar = document.getElementById("file-progress"),
-      fileSizeLimit = 1024; // In MB
-    if (xhr.upload) {
-      // Check if file is less than x MB
-      if (file.size <= fileSizeLimit * 1024 * 1024) {
-        // Progress bar
-        pBar.style.display = "inline";
-        xhr.upload.addEventListener("loadstart", setProgressMaxValue, false);
-        xhr.upload.addEventListener("progress", updateFileProgress, false);
-
-        // File received / failed
-        xhr.onreadystatechange = function (e) {
-          if (xhr.readyState == 4) {
-            // Everything is good!
-            // progress.className = (xhr.status == 200 ? "success" : "failure");
-            // document.location.reload(true);
-          }
-        };
-
-        // Start upload
-        xhr.open(
-          "POST",
-          document.getElementById("file-upload-form").action,
-          true
-        );
-        xhr.setRequestHeader("X-File-Name", file.name);
-        xhr.setRequestHeader("X-File-Size", file.size);
-        xhr.setRequestHeader("Content-Type", "multipart/form-data");
-        xhr.send(file);
-      } else {
-        output("Please upload a smaller file (< " + fileSizeLimit + " MB).");
-      }
-    }
-  }
-
-  // Check for the various File API support.
-  if (window.File && window.FileList && window.FileReader) {
-    Init();
-  } else {
-    document.getElementById("file-drag").style.display = "none";
-  }
-}
-ekUpload();
-
-function drawImageScaled(img, ctx) {
-  const canvas = ctx.canvas;
-  const hRatio = canvas.width / img.width;
-  const vRatio = canvas.height / img.height;
-  const ratio = Math.min(hRatio, vRatio);
-  const centerShift_x = (canvas.width - img.width * ratio) / 2;
-  const centerShift_y = (canvas.height - img.height * ratio) / 2;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(
-    img,
-    0,
-    0,
-    img.width,
-    img.height,
-    centerShift_x,
-    centerShift_y,
-    img.width * ratio,
-    img.height * ratio
-  );
-}
+import {
+  downloadCanvasImage,
+  drawImageOnCanvas,
+  drawTextOnCanvas,
+  ekUpload,
+} from "./functions.js";
 
 // Canvas
 
-const form = document.getElementById("file-upload-form");
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-});
-/**
- * Dessine une image dans un canvas
- * @author NdekoCode
- * @param {string} url
- * @param {HTMLCanvasElement} canvas
- * @param {CanvasRenderingContext2D} ctx
- */
-function drawImageOnCanvas(url, canvas, ctx) {
-  const image = new Image();
-  image.addEventListener("load", () => {
-    const maxWidth = canvas.width;
-    const maxHeight = canvas.height;
-    const imageWidth = image.naturalWidth;
-    const imageHeight = image.naturalHeight;
-
-    // calculate the maximum dimensions while preserving aspect ratio
-    let width = maxWidth;
-    let height = maxHeight;
-    if (imageWidth > maxWidth || imageHeight > maxHeight) {
-      const ratio = Math.min(maxWidth / imageWidth, maxHeight / imageHeight);
-      width = Math.floor(imageWidth * ratio);
-      height = Math.floor(imageHeight * ratio);
-    }
-
-    // center the image in the canvas
-    const xMargin = Math.floor((maxWidth - width) / 2);
-    const yMargin = Math.floor((maxHeight - height) / 2);
-
-    ctx.drawImage(image, xMargin, yMargin, width, height);
-  });
-  image.src = url;
-}
 /** @type {HTMLCanvasElement} */
+const formContent = { imageUrl: "", textCanvas: "" };
 const canvas = document.getElementById("canvas");
+const downloadBtn = document.getElementById("btn-download");
+
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 const imageFileUpload = document.getElementById("file-upload");
+const text = document.getElementById("title");
+const form = document.getElementById("file-upload-form");
+
 imageFileUpload.addEventListener("change", function (evt) {
   const image = imageFileUpload.files[0];
   /**@type {HTMLImageElement} */
   const imageURL = URL.createObjectURL(image);
   drawImageOnCanvas(imageURL, canvas, ctx);
 });
-const text = document.getElementById("title");
-text.addEventListener("input", (evt) => {
+
+text.addEventListener("keydown", (evt) => {
   let value = evt.currentTarget.value;
-  drawTextOnCanvas(text, canvas, ctx);
-  console.log(value);
+  formContent.textCanvas = value;
 });
+
+downloadBtn.addEventListener("click", () => downloadCanvasImage(canvas));
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  if (formContent.textContent && formContent.textContent.trim().length > 0) {
+    drawTextOnCanvas(formContent.textCanvas, canvas, ctx);
+  }
+});
+
+// File Upload
+ekUpload();
