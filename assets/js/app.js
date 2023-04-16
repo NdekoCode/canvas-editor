5d25522d8f164ace5f7d70f318dbc63a4d6b45fc
NdekoCode <arickbulakali@gmail.com>
2023-04-17 01:59:35 +0200

    style: add responsive editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,6 @@ import {
 // Canvas
 
 /** @type {HTMLCanvasElement} */
-const formContent = { imageUrl: "", textCanvas: "" };
 const canvas = document.getElementById("canvas");
 const downloadBtn = document.getElementById("btn-download");
 
@@ -17,7 +16,7 @@ const ctx = canvas.getContext("2d");
 const imageFileUpload = document.getElementById("file-upload");
 const text = document.getElementById("title");
 const form = document.getElementById("file-upload-form");
-
+const formContent = { textCanvasColor: "", textCanvas: "" };
 imageFileUpload.addEventListener("change", function (evt) {
   const image = imageFileUpload.files[0];
   /**@type {HTMLImageElement} */
@@ -25,17 +24,29 @@ imageFileUpload.addEventListener("change", function (evt) {
   drawImageOnCanvas(imageURL, canvas, ctx);
 });
 
-text.addEventListener("keydown", (evt) => {
+text.addEventListener("input", (evt) => {
   let value = evt.currentTarget.value;
   formContent.textCanvas = value;
 });
 
 downloadBtn.addEventListener("click", () => downloadCanvasImage(canvas));
 
+let colorInput = document.querySelector("#color");
+let hexInput = document.querySelector("#hex");
+colorInput.addEventListener("input", () => {
+  let color = colorInput.value;
+  hexInput.value = color;
+  formContent.textCanvasColor = color;
+});
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  if (formContent.textContent && formContent.textContent.trim().length > 0) {
-    drawTextOnCanvas(formContent.textCanvas, canvas, ctx);
+  console.log(
+    formContent.textCanvas,
+    formContent.textCanvas && formContent.textCanvas.trim().length > 0
+  );
+  if (formContent.textCanvas && formContent.textCanvas.trim().length > 0) {
+    drawTextOnCanvas(formContent, canvas, ctx);
+    console.log(formContent);
   }
 });
 
